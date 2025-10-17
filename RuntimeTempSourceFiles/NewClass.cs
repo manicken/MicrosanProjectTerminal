9433733e4d75d31bed80df8c80b898038ef7f86e
Jannik Svensson <tde04014@gmail.com>
2025-10-17 18:09:05 +0200

    RuntimeProgramming now support virtual files needed for this project

---
+++ RuntimeTempSourceFiles/NewClass.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+using System.Windows.Forms;
+using Microsan;
+
+//using OxyPlot;
+//using OxyPlot.Series;
+//using OxyPlot.WindowsForms;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.IO.Ports;
+
+namespace RuntimeProgrammingNamespace
+{ 
+    public class NewClass
+    {
+        private static object RootObject = null;
+        
+        public static void NewMethod(object rootObject)
+        {
+            RootObject = rootObject;
+            
+            int i = 0;
+            MessageBox.Show("hello" + 5/i);
+        }
+    }
+}
